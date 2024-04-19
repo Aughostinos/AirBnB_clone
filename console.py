9b9e1ab5f8baa7d9b68a3da908a63fb5497e75cf
aughostinos <augustinos.nabil@gmail.com>
2024-04-18 20:31:06 -0700

    create

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """contains the entry point of the command interpreter"""
 import cmd
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
@@ -18,6 +19,22 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         'handle empty line input'
         pass
+
+    def do_create(self, arg):
+        'create new class'
+    args = arg.split()
+    if len(args) == 0:
+        print("** class name missing **")
+        return
+    class_name = args[0]
+    try:
+        cls = globals()[class_name]
+    except KeyError:
+        print("** class doesn't exist **")
+        return
+    instance = cls()
+    instance.save()
+    print(instance.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
