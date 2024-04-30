53889f22805c3714bf4a27dbe431fc7360ee1325
aughostinos <augustinos.nabil@gmail.com>
2024-04-29 17:51:19 -0700

    fix

--- console.py
+++ console.py
@@ -148,7 +148,7 @@ class HBNBCommand(cmd.Cmd):
             attr_type = type(getattr(instance, attribute_name, str))
             casted_value = attr_type(attribute_value)
             setattr(instance, attribute_name, casted_value)
-            models.storage.save()
+            instance.save()
 
 
 if __name__ == '__main__':
