73adbbf38f7117444cf11a70be1e7aa56f88c86f
Augustinos <augustinos.nabil@gmail.com>
2024-05-01 14:28:51 -0700

    test_user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,6 +4,7 @@
 from models.user import User
 import unittest
 
-def test_attribute(user, attribute, expected):
-    actual = getattr(user, attribute)
-    assert actual == expected, f"Test Failed: User.{attribute} is '{actual}' but expected '{expected}'"
+class TestUser(unittest.testcase):
+    def test_attribute(user, attribute, expected):
+        actual = getattr(user, attribute)
+        assert actual == expected, f"Test Failed: User.{attribute} is '{actual}' but expected '{expected}'"
