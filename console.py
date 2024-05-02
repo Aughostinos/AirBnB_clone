2f4b49755cc7d22b2cc91032ad4f79f3c6a343c2
Augustinos <augustinos.nabil@gmail.com>
2024-05-01 17:04:25 -0700

    update console

--- console.py
+++ console.py
@@ -5,6 +5,11 @@ import sys
 import models
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 from models.__init__ import storage
 
 
